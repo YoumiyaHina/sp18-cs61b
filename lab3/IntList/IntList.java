04b8d6b02ee6934b12b81d9f8d3dce3a8a581edd
hina <YomiyaHina@users.noreply.github.com>
2023-11-07 22:21:47 +0800

    lab3 complete testReverse

--- lab3/IntList/IntList.java
+++ lab3/IntList/IntList.java
@@ -246,5 +246,8 @@ public class IntList {
         out.format(")");
         return out.toString();
     }
+    public static IntList reverse(IntList A) {
+        return null;
+    }
 }
 
--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -69,5 +69,12 @@ public class IntListTest {
     /** If you're running this from the command line, you'll need
       * to add a main method. See ArithmeticTest.java for an
       * example. */
+    @Test(timeout = 1000)
+    public  void testReverse(){
+        IntList A=IntList.of(1,2,3,4);
+        assertEquals(IntList.of(4,3,2,1),IntList.reverse(A));
+        assertNotEquals(IntList.of(1,2,3,4),A);//silly test
+        assertEquals(null,IntList.reverse(null));
+    }
 
 }
